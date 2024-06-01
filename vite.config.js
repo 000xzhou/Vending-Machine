1081a9f2304678b2d05e2b52e3939a0b0ee4dd4f
000xzhou <xzhou92@gmail.com>
2024-05-31 23:58:43 -0400

    let's get rid of base and try some where else

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "Vending-Machine",
+  // base: "/Vending-Machine/",
 });
